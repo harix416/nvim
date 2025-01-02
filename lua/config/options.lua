6e6c41d963ccfe26fea2ce4036e74625a46325a8
Jingxiang Qi <jackinqi@gmail.com>
2025-01-02 14:51:03 +0800

    add some plugins & set options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+-- 禁用拼写检查
+opt.spell = false
+
+-- 禁用相对行号
+opt.relativenumber = false
